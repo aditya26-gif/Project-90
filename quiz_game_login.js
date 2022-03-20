a2d9d80610360c1e85cd3ec35514315926494f21
aditya26-gif <76239749+aditya26-gif@users.noreply.github.com>
2022-03-20 23:00:19 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+    p1_name = document.getElementById("p1_name").value;
+    p2_name = document.getElementById("p2_name").value;
+    localStorage.setItem("p1_name", p1_name);
+    localStorage.setItem("p2_name", p2_name);
+    window.location = "quiz_game_page.html";
+}
